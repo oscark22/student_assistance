3afc10e6504cfe4c20c58d14293558cfc9264a9f
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-09-15 10:21:28 -0700

    started with the project

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -11,12 +11,12 @@
 	</div>
 
 	<nav data-sveltekit-prefetch>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}>
 				<a href="/">Home</a>
+			</li>
+			<li class:active={$page.url.pathname === '/dashboard'}>
+				<a href="/dashboard">About</a>
 			</li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a href="/about">About</a>
@@ -28,11 +28,10 @@
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
-	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+		</nav>
+		<div class="corner">
+			<!-- TODO put something else here? github link? -->
+		</div>
 </header>
 
 <style>
