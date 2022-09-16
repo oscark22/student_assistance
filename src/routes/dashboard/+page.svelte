4e53766075670dd37895e6292adc68a3fff39a95
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-09-15 17:25:56 -0700

    added a new dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   let groupNum:number = 8;
   let totAbsences:number = 9;
-  let threeStudentsMoreAbsences:string[] = ["Genovefa Yaakov", "Amlaíb Quirino", "Janina Aslan"];
-  let noTestRight:string[] = ['Rusen Puck', 'Küllike Thracius', 'Jeffery Ignàtziu', 'Matic Blahoslav', 'Diana Eulalia'];
-  let ordinary:string[] = ['Romana Borghild', 'Küllike Thracius', 'Jeffery Ignàtziu'];
-  let extraordinary:string[] = ['Küllike Thracius'];
+  let threeStudentsMoreAbsences:string[]|null = ["Genovefa Yaakov", "Amlaíb Quirino", "Janina Aslan"];
+  let noTestRight:string[]|null = ['Rusen Puck', 'Küllike Thracius', 'Jeffery Ignàtziu', 'Matic Blahoslav', 'Diana Eulalia'];
+  let ordinary:string[]|null = ['Romana Borghild', 'Küllike Thracius', 'Jeffery Ignàtziu'];
+  let extraordinary:string[]|null = ['Küllike Thracius'];
+
+  let maxLen:number = Math.max(threeStudentsMoreAbsences.length, noTestRight.length, ordinary.length, extraordinary.length);
 </script>
 
 <div class="d-flex justify-content-center">
@@ -12,8 +14,8 @@
     Dashboard
   </h1>
 </div>
-<div class="mt-4">
-  <table class="table">
+<div class="d-flex px-4 mt-4">
+  <table class="table table-striped table-bordered">
     <thead>
       <tr>
         <th scope="col">Num. Grupo</th>
@@ -22,23 +24,83 @@
         <th scope="col">Sin Derecho (Examen)</th>
         <th scope="col">Ordinario (20% Faltas)</th>
         <th scope="col">Extraordinario (30% Faltas)</th>
+        <th scope="col" class="text-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+          </svg>
+        </th>
+        <th scope="col" class="text-center">M</th>
+        <th scope="col" class="text-center">E</th>
       </tr>
     </thead>
     <tbody>
+      <!-- TODO each per group -->
       <tr>
-        <th scope="row" rowspan="5">1</th>
-        <td rowspan="5">1</td>
-        <td rowspan="1">1</td>
-        <td rowspan="1">1</td>
-        <td rowspan="1">1</td>
-        <td rowspan="1">1</td>
+        <th scope="row" rowspan="{maxLen}">1</th>
+        <td rowspan="{maxLen}">13</td>
+        {#if Array.isArray(threeStudentsMoreAbsences)}
+          <td>{threeStudentsMoreAbsences[0]}</td>
+        {:else}
+          <td>Nothing</td>
+        {/if}
+        {#if Array.isArray(noTestRight)}
+          <td>{noTestRight[0]}</td>
+        {:else}
+          <td>Nothing</td>
+        {/if}
+        {#if Array.isArray(ordinary)}
+          <td>{ordinary[0]}</td>
+        {:else}
+          <td>Nothing</td>
+        {/if}
+        {#if Array.isArray(extraordinary)}
+          <td>{extraordinary[0]}</td>
+        {:else}
+          <td>Nothing</td>
+        {/if}
+        <td rowspan="{maxLen}" class="text-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+          </svg>
+        </td>
+        <td rowspan="{maxLen}" class="text-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+          </svg>
+        </td>
+        <td rowspan="{maxLen}" class="text-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+          </svg>
+        </td>
       </tr>
-      <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-      </tr>
+      {#each Array(maxLen-1) as _, i (i)}
+        <tr>
+          {#if Array.isArray(threeStudentsMoreAbsences) && i+1 < threeStudentsMoreAbsences.length}
+            <td>{threeStudentsMoreAbsences[i+1]}</td>
+          {:else}
+            <td></td>
+          {/if}
+          {#if Array.isArray(noTestRight) && i+1 < noTestRight.length}
+            <td>{noTestRight[i+1]}</td>
+          {:else}
+            <td></td>
+          {/if}
+          {#if Array.isArray(ordinary) && i+1 < ordinary.length}
+            <td>{ordinary[i+1]}</td>
+          {:else}
+            <td></td>
+          {/if}
+          {#if Array.isArray(extraordinary) && i+1 < extraordinary.length}
+            <td>{extraordinary[i+1]}</td>
+          {:else}
+            <td></td>
+          {/if}
+        </tr>
+      {/each}
+      <!-- END  -->
     </tbody>
   </table>
 </div>
