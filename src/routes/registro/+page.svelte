4458e50143e9dd68684f36d485443722adfdd1f2
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-09-23 12:27:24 -0700

    Modal quitado, forms de alumno materia y grupo

--- src/routes/registro/+page.svelte
+++ src/routes/registro/+page.svelte
@@ -1,7 +1,42 @@
 <script>
-	import Alumno from "./alumno.svelte";
+	import Alumno from './alumno.svelte';
+	import Materia from './materia.svelte';
+	import Grupo from './grupo.svelte';
+
+	let alum = false;
+	let group = false;
+	let mat = false;
+
+
+	function change_alum(){
+		alum = true;
+		group = false;
+		mat = false;
+	}
+	function change_mat(){
+		alum = false;
+		group = false;
+		mat = true;
+	}
+	function change_group(){
+		alum = false;
+		group = true;
+		mat = false;
+	}
 </script>
 
-<main>
+<button type="button" class="btn btn-outline-primary col-3 mx-5" on:click={() => change_mat()}>Registrar Materia</button>
+
+<button type="button" class="btn btn-outline-primary col-3 mx-5" on:click={() => change_group()}>Registrar Grupo</button>
+
+<button type="button" class="btn btn-outline-primary col-3 mx-5" on:click={() => change_alum()}>Registrar Alumno</button>
+
+{#if alum}
 	<Alumno />
-</main>
+{/if}
+{#if mat}
+	<Materia />
+{/if}
+{#if group}
+	<Grupo />
+{/if}
