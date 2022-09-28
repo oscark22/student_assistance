19af9dc1f5ca6f98622247bde7c0d4f2bb734933
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-09-28 10:30:58 -0700

    gridjs now in dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,17 +1,66 @@
 <script lang="ts">
-  import Grid from "gridjs-svelte"
+	import { html } from "gridjs";
+  import Grid from "gridjs-svelte";
 
-  const data = [
-    { name: "John", email: "john@example.com" },
-    { name: "Mark", email: "mark@gmail.com" },
+  const columns = [
+    "Num. Grupo",
+    "Tot. Faltas",
+    { 
+      name: "3 Alumnos + Faltas",
+      sort: {
+        enabled: false
+      }
+    },
+    { 
+      name: "Sin Derecho (Examen)",
+      sort: {
+        enabled: false
+      }
+    },
+    { 
+      name: "Ordinario (20% Faltas)",
+      sort: {
+        enabled: false
+      }
+    },
+    { 
+      name: "Extraordinario (30% Faltas)",
+      sort: {
+        enabled: false
+      }
+    },
+    { 
+      name: "Acciones",
+      formatter: (cell: string) => html(`<a href="${cell}">Ver</a> | <a href="${cell}">Editar</a>`),
+      sort: {
+        enabled: false
+      },
+      width: "10%"
+    },
   ]
-
+    
   let groupNum:number = 8;
   let totAbsences:number = 9;
   let threeStudentsMoreAbsences:string[]|null = ["Genovefa Yaakov", "Amlaíb Quirino", "Janina Aslan"];
   let noTestRight:string[]|null = ['Rusen Puck', 'Küllike Thracius', 'Jeffery Ignàtziu', 'Matic Blahoslav', 'Diana Eulalia'];
   let ordinary:string[]|null = ['Romana Borghild', 'Küllike Thracius', 'Jeffery Ignàtziu'];
   let extraordinary:string[]|null = ['Küllike Thracius'];
+
+  const data = [
+    ['1', '13', threeStudentsMoreAbsences.join(", "), noTestRight.join(", "), ordinary.join(", "), extraordinary.join(", ")]
+  ]
+
+  const pagination = {
+    enabled: true,
+    limit: 10,
+    summary: false
+  }
+
+  const search = {
+    enabled: true
+  }
+
+  const sort = true;
 
   let maxLen:number = Math.max(threeStudentsMoreAbsences.length, noTestRight.length, ordinary.length, extraordinary.length);
 </script>
@@ -21,92 +70,6 @@
     Dashboard
   </h1>
 </div>
-<div class="d-flex mt-4">
-  <table class="table table-striped table-bordered">
-    <thead>
-      <tr>
-        <th scope="col">Num. Grupo</th>
-        <th scope="col">Tot. Faltas</th>
-        <th scope="col">3 Alumnos + Faltas</th>
-        <th scope="col">Sin Derecho (Examen)</th>
-        <th scope="col">Ordinario (20% Faltas)</th>
-        <th scope="col">Extraordinario (30% Faltas)</th>
-        <th scope="col" class="text-center">
-        </th>
-        <th scope="col" class="text-center">
-        </th>
-        <th scope="col" class="text-center">
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- TODO each per group -->
-      <tr>
-        <th scope="row" rowspan="{maxLen}">1</th>
-        <td rowspan="{maxLen}">13</td>
-        {#if Array.isArray(threeStudentsMoreAbsences)}
-          <td>{threeStudentsMoreAbsences[0]}</td>
-        {:else}
-          <td>Nothing</td>
-        {/if}
-        {#if Array.isArray(noTestRight)}
-          <td>{noTestRight[0]}</td>
-        {:else}
-          <td>Nothing</td>
-        {/if}
-        {#if Array.isArray(ordinary)}
-          <td>{ordinary[0]}</td>
-        {:else}
-          <td>Nothing</td>
-        {/if}
-        {#if Array.isArray(extraordinary)}
-          <td>{extraordinary[0]}</td>
-        {:else}
-          <td>Nothing</td>
-        {/if}
-        <td rowspan="{maxLen}" class="text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-          </svg>
-        </td>
-        <td rowspan="{maxLen}" class="text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-          </svg>
-        </td>
-        <td rowspan="{maxLen}" class="text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-          </svg>
-        </td>
-      </tr>
-      {#each Array(maxLen-1) as _, i (i)}
-        <tr>
-          {#if Array.isArray(threeStudentsMoreAbsences) && i+1 < threeStudentsMoreAbsences.length}
-            <td>{threeStudentsMoreAbsences[i+1]}</td>
-          {:else}
-            <td></td>
-          {/if}
-          {#if Array.isArray(noTestRight) && i+1 < noTestRight.length}
-            <td>{noTestRight[i+1]}</td>
-          {:else}
-            <td></td>
-          {/if}
-          {#if Array.isArray(ordinary) && i+1 < ordinary.length}
-            <td>{ordinary[i+1]}</td>
-          {:else}
-            <td></td>
-          {/if}
-          {#if Array.isArray(extraordinary) && i+1 < extraordinary.length}
-            <td>{extraordinary[i+1]}</td>
-          {:else}
-            <td></td>
-          {/if}
-        </tr>
-      {/each}
-      <!-- END  -->
-    </tbody>
-  </table>
+<div class="d-flex">
+  <Grid {columns} {data} {pagination} {search} {sort} />
 </div>
